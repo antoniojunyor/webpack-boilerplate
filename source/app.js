c4f9816390cecd58b95abfb2b1b08217952d5765
Antonio Junior <junyor@hotmail.co.uk>
2017-02-27 22:21:09 -0300

    Adding CSS, SASS loaders and generating output CSS file.

--- source/app.js
+++ source/app.js
@@ -1,3 +1,4 @@
+var css = require('./app.sass');
 var message = 'webpack starter boilerplate';
 
 console.log(message);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,21 @@
+'use strict';
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const html_webpack_plugin = new HtmlWebpackPlugin({
+  title: 'Webpack boilerplate',
+  minify: {
+    collapseWhitespace: true
+  },
+  hash: true,
+  template: './source/index.html'
+});
+
+const extract_sass = new ExtractTextPlugin({
+  filename: 'app.css',
+  disable: false,
+  allChunks: true
+})
 
 module.exports = {
   entry: './source/app.js',
@@ -6,14 +23,27 @@ module.exports = {
     path: 'build',
     filename: 'app.bundle.js'
   },
+  module: {
+    rules: [{
+      test: /\.sass$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [{
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+            minimize: true
+          }
+        }, {
+          loader: 'sass-loader'
+        }],
+        publicPath: '/build'
+      })
+    }]
+  },
+  devtool: 'source-map',
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'webpack boilerplate',
-      minify: {
-        collapseWhitespace: true
-      },
-      hash: true,
-      template: './source/index.html'
-    })
+    html_webpack_plugin,
+    extract_sass
   ]
 }
