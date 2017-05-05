acdf8a93cdc71e8121b7052fc44a180a3a12524e
Antonio Junior <junyor@hotmail.co.uk>
2017-05-04 22:23:28 -0300

    Improvements on web pack config accord environment.

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,93 @@
 'use strict';
 var ExtractTextPlugin = require('extract-text-webpack-plugin'),
     HtmlWebpackPlugin = require('html-webpack-plugin'),
-    path = require('path');
+    webpack = require('webpack'),
+    imageWebpackLoader = require('image-webpack-loader'),
+    path = require('path'),
+    is_prod = process.env.NODE_ENV === 'production', // true or false
+    css_dev = ['style-loader', 'css-loader', 'sass-loader'],
+    css_prod = ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: ['css-loader', 'sass-loader'],
+      publicPath: '/build'
+    }),
+    css_config = is_prod ? css_prod : css_dev;
 
 const html_webpack_plugin = new HtmlWebpackPlugin({
   title: 'Webpack boilerplate',
-  minify: { collapseWhitespace: true },
   hash: true,
   template: './source/index.html'
 });
 
 const extract_sass = new ExtractTextPlugin({
   filename: 'app.css',
-  disable: false,
+  disable: !is_prod,
   allChunks: true
 })
 
 module.exports = {
-  entry: './source/app.js',
+  entry: {
+    app: './source/app.js'
+  },
   output: {
-    path: '/',
-    filename: 'app.bundle.js'
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].bundle.js'
   },
   module: {
-    rules: [{
-      test: /\.sass$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-            minimize: true
+    rules: [
+      {
+        test: /\.sass$/,
+        use: css_config
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader?name=[name].[ext]?publicPath=assets/images/&outputPath=assets/images/',
+            query: {
+              useRelativePath: is_prod
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              mozjpeg: {
+                progressive: true,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 4,
+              },
+              pngquant: {
+                quality: '75-90',
+                speed: 3,
+              },
+            },
           }
-        }, {
-          loader: 'sass-loader',
-          options: { sourceMap: true }
-        }],
-        publicPath: '/build'
-      })
-    }]
+        ],
+        exclude: /node_modules/,
+        include: __dirname,
+      }
+    ]
   },
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'build'),
     compress: true,
+    hot: true,
     port: 9000
   },
   plugins: [
     html_webpack_plugin,
-    extract_sass
+    extract_sass,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
   ]
 }
